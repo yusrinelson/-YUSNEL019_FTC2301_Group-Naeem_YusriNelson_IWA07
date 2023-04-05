c458407a21b39d2d287bb88dff6f07265eb7a753
yusrinelson <125978974+yusrinelson@users.noreply.github.com>
2023-04-05 16:23:44 +0200

    added comments and changed - to +

--- js/challenge-3.js
+++ js/challenge-3.js
@@ -10,15 +10,15 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = "R " + (parseInt(leoBalance) + parseInt(sarahBalance))
-const leo = `${leoName} ${leoSurname} (Owed: R  ${(leoBalance)})`
-const sarah =`${sarahName} ${sarahSurname} (Owed: R  ${Number.parseFloat(sarahBalance ).toFixed(2)})`
+const owed = "R " + (parseInt(leoBalance * -1) + parseInt(sarahBalance * -1)).toFixed(2) //added * -1 to create +
+const leo = `${leoName} ${leoSurname} (Owed: R  ${(leoBalance * -1).toFixed(2)})`
+const sarah =`${sarahName} ${sarahSurname} (Owed: R  ${(sarahBalance * -1).toFixed(2)})` //gives decimal
 const total = "Total amount owed: "
-const result = `${leo} 
+const result = `${leo}
 ${sarah} 
 
 ${divider} 
-    ${total}  ${owed}
-${divider} `
+    ${total}  ${(owed)}
+${divider} ` //used backtick to adjust layout
 
 console.log(result)
